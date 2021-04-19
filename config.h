c437cfdc338e46b226bde1757820b47179006e2d
paga2004 <pascigamma@bluewin.ch>
2021-04-19 13:12:39 +0200

    Change keybinding to swap colors

--- config.h
+++ config.h
@@ -230,7 +230,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ XK_ANY_MOD,           XK_F6,          swapcolors,     {.i =  0} },
+	{ TERMMOD,              XK_F6,          swapcolors,     {.i =  0} },
 };
 
 /*
